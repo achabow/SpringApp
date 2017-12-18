f460133ad28c78a0f1612364882da07ace8dc334
ac <imac@192.168.1.2>
2017-12-18 21:30:12 +0100

    made model class and table in db

---
+++ src/main/java/model/User.java
@@ -0,0 +1,44 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USER")
+public class User {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int user_id;
+	private String email;
+	private String password;
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString(){
+		return "user_id"+user_id+ ", email "+email;
+	}
+	
+}
